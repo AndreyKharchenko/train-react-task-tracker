78f92caf4d7295aa040188eb4b8c7d13b15a91a5
Andrey <KharchenkoAndru@gmail.com>
2023-09-05 23:05:35 +0300

    Updates

--- src/hooks/useStore.ts
+++
@@ -1,4 +0,0 @@
-import { useContext } from "react";
-import { rootStoreContext } from "../store/index";
-
-export const useStore = () => useContext(rootStoreContext);
--- src/store/index.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-import  TodoStore  from "./todos";
-
-export const rootStoreContext = createContext({
-  todoStore: TodoStore
-});
